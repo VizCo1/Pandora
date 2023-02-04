0041c9363f8ef8299ee5816dce87a3214b769afb
Pitarcus <pitarcusmp@gmail.com>
2023-02-04 15:39:19 +0100

    Movimiento y animacines

--- Assets/Scripts/ControlJugador.cs
+++ Assets/Scripts/ControlJugador.cs
@@ -6,6 +6,7 @@ public class ControlJugador : MonoBehaviour
     
     private Animator anim;
     [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private SpriteRenderer sr;
 
     [SerializeField]
     private float velocidad = 5.0f;
@@ -34,7 +35,28 @@ public class ControlJugador : MonoBehaviour
         Vector3 move = new Vector2(movimientoInput.x, movimientoInput.y);
 
         move = move.normalized;
-        //rb.MovePosition(rb.transform.position + move * velocidad * Time.deltaTime);
+        //rb.MovePosition(rb.transform.position + move * velocidad * Time.deltaTime); 
+
+        if (move != Vector3.zero) {
+
+            anim.SetFloat("moveX", move.x);
+            anim.SetFloat("moveY", move.y);
+
+            anim.SetBool("moving", true);
+
+            if (move.x > 0)
+            {
+                sr.flipX = true;
+            }
+            else
+            {
+                sr.flipX = false;
+            }
+        }
+        else
+        {
+            anim.SetBool("moving", false);
+        }
 
         rb.velocity = Vector2.Lerp(rb.velocity, move * velocidad, smoothing);
     }
